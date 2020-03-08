115b7a74d840fada6e8e41fb4f5e6c220e680053
kiprasv <61943967+kiprasv@users.noreply.github.com>
2020-03-08 22:32:59 +0400

    Create list.js

---
+++ list.js
@@ -0,0 +1,6 @@
+[
+  {"name":"ak-43","float":0.15,"price":151515},
+   {"name":"ak-43","float":0.15,"price":151515},
+  {"name":"ak-43","float":0.15,"price":151515},
+    {"name":"ak-43","float":0.15,"price":151515}
+]
