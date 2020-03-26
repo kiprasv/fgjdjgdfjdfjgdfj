5fa45e94a537c6d0b20167e836a52443b63b0557
kiprasv <61943967+kiprasv@users.noreply.github.com>
2020-03-27 01:20:55 +0400

    Update list.js

--- list.js
+++ list.js
@@ -4,11 +4,4 @@
     "name": "AK-47 | Redline (Field-Tested)",
     "float": 0.99,
     "price": 300
-},
-{
-
-    "name": "StatTrak™ Desert Eagle | Mecha Industries (Field-Tested)",
-    "float": 0.159,
-    "price": 300
-}
-    ]
+},]
